b940834f76b51302d6e456a3e8dcaaecb37b9a9e
DanaGolub <48007635+DanaGolub@users.noreply.github.com>
2021-12-28 16:54:36 -0800

    before introducting a global state to access favs

--- src/pages/PageFavs.js
+++ src/pages/PageFavs.js
@@ -1,19 +1,20 @@
 import { useState, useEffect } from 'react';
 import AddingFavs from '../components/AddingFavs';
 import IndivMovie from '../components/IndivMovie';
+import MovieCard from '../components/MovieCard';
 import PageSingleMovie from './PageSingleMovie';
 
-function PageFavs(selectedFavs) {
+function PageFavs() {
 
     //const [favourites, setFavourites] = useState([]);
-    const favs = selectedFavs
-    console.log(favs)
+    // const favs = selectedFavs
+    // console.log(favs)
 
     return (
         <section className="favs-page">
             <h2>Page Favs</h2>
             
-            {/* <IndivMovie /> */}
+            {/* <Movies /> */}
         </section>
     )
 }
--- src/pages/PageSingleMovie.js
+++ src/pages/PageSingleMovie.js
@@ -5,12 +5,13 @@ import IndivMovie from '../components/IndivMovie';
 import MovieCard from "../components/MovieCard";
 import AddingFavs from '../components/AddingFavs';
 import API_KEY from '../globals/globals';
+import Movies from '../components/Movies';
 
 
 function PageSingleMovie() {
 
     const [movieData, setMovieData] = useState(null);
-    const [favourites, setFavourites] = useState([]);
+    const [favourites, setFavourites] = useState([null]);
 
     const { id } = useParams();
 
@@ -18,25 +19,28 @@ function PageSingleMovie() {
         const fetchMovies = async () => {
             const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`);
             const data = await res.json();
-            console.log(data)
+            //console.log(data)
             setMovieData(data);
         }
         fetchMovies();
     }, [id])
 
     const addFavMovie = (movie) => {
-        console.log(movie)
-        console.log("testing this")
         const newFavList = [...favourites, movie]
         setFavourites(newFavList)
     }
 
     return (
+        <div>
         <section className="single-movie-page">
            {/* { movieData !== null &&  <h2>{movieData.title}</h2> } */}
-           {movieData !== null && <IndivMovie movie={movieData} key={movieData.id} favouriteComponent={AddingFavs} handlingFavsClick={addFavMovie} selectedFavs={favourites}/>}
+           {movieData !== null && <IndivMovie movie={movieData} key={movieData.id} favouriteComponent={AddingFavs} handlingFavsClick={addFavMovie}/>}
+           {/* {favourites !== null && <Movies moviesData={favourites} />} */}
+           {console.log(favourites)}
         </section>
+        </div>
     )
 }
 
 export default PageSingleMovie
+// selectedFavs={favourites}
