0f610eac3a993968267715d987f4f96920ac5fd0
DanaGolub <48007635+DanaGolub@users.noreply.github.com>
2021-12-30 15:40:29 -0800

    same message as prev commit

--- src/components/IndivMovie.js
+++ src/components/IndivMovie.js
@@ -2,22 +2,20 @@ import { Link } from 'react-router-dom';
 import React, { useState, useContext } from 'react';
 import noPoster from '../images/no-movie-poster.jpg';
 import AddingFavs from '../components/AddingFavs';
-import { GlobalContext } from '../context/GlobalState';
 import RemovingFavs from './RemovingFavs';
+
+import { GlobalContext } from '../context/GlobalState';
 
 
 
 function IndivMovie({ movie, favouriteComponent, handlingFavsClick }) {
 
     const FavComponent = favouriteComponent;
-    // const { addMovieToFavs, removeMovieFromFavs, favourites } = useContext(GlobalContext); 
+    const { addMovieToFavs, removeMovieFromFavs, favourites } = useContext(GlobalContext);
 
-    // let storedFavMovie = favourites.find(movieObj => movieObj.id === movie.id);
+    let storedFavMovie = favourites.find(movieObj => movieObj.id === movie.id);
+    let disableButton = storedFavMovie ? true : false;
 
-    //const addOrRemoveFavs = storedFavMovie ? true : false;
-    // console.log(movie.id)
-    // console.log(storedFavMovie.id)
-    
 
     return (
         <div className="movie-card">
@@ -27,16 +25,30 @@ function IndivMovie({ movie, favouriteComponent, handlingFavsClick }) {
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
                 }
             </div>
-            <div onClick={() => handlingFavsClick(movie)} className='movie-info'>
-                <h3>{movie.title}</h3>
-                <h3>{movie.release_date}</h3>
-                <h3>{movie.vote_average}</h3>
-                <h3>{movie.overview}</h3>
-                <FavComponent />
-                {/* <AddingFavs /> */}
-            </div>
+
+            {!storedFavMovie ? (
+                <div onClick={() => addMovieToFavs(movie)} className='movie-info'>
+                    <h3>{movie.title}</h3>
+                    <h3>{movie.release_date}</h3>
+                    <h3>{movie.vote_average}</h3>
+                    <h3>{movie.overview}</h3>
+                    <AddingFavs />
+                </div>
+            )
+                :
+                (
+                    <div onClick={() => removeMovieFromFavs(movie)} className='movie-info'>
+                        <h3>{movie.title}</h3>
+                        <h3>{movie.release_date}</h3>
+                        <h3>{movie.vote_average}</h3>
+                        <h3>{movie.overview}</h3>
+                        <RemovingFavs />
+                    </div>
+                )
+            }
         </div>
     )
 }
-    
+
+
 export default IndivMovie
