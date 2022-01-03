ac518d91852e6f92a494ff97743e5e97281997de
DanaGolub <48007635+DanaGolub@users.noreply.github.com>
2022-01-02 21:54:36 -0800

    before completely modifying the indivMovie component, working on overlay effect

--- src/components/IndivMovie.js
+++ src/components/IndivMovie.js
@@ -8,13 +8,13 @@ import { GlobalContext } from '../context/GlobalState';
 
 
 
-function IndivMovie({ movie, favouriteComponent, handlingFavsClick }) {
+function IndivMovie({ movie, favouriteComponent }) {
 
-    const FavComponent = favouriteComponent;
+    // const FavComponent = favouriteComponent;
     const { addMovieToFavs, removeMovieFromFavs, favourites } = useContext(GlobalContext);
 
     let storedFavMovie = favourites.find(movieObj => movieObj.id === movie.id);
-    let disableButton = storedFavMovie ? true : false;
+    // let disableButton = storedFavMovie ? true : false;
 
 
     return (
@@ -32,6 +32,9 @@ function IndivMovie({ movie, favouriteComponent, handlingFavsClick }) {
                     <h3>{movie.release_date}</h3>
                     <h3>{movie.vote_average}</h3>
                     <h3>{movie.overview}</h3>
+                    <div className="overlay d-flex align-items-center justify-content">
+
+                    </div>
                     <AddingFavs />
                 </div>
             )
--- src/components/RemovingFavs.js
+++ src/components/RemovingFavs.js
@@ -3,11 +3,11 @@ import React from 'react';
 function RemovingFavs() {
     return (
         <div>
-            <button className=''>Remove From Your Favourites
+            <button className='button-favs'>Remove From Your Favourites
             <svg 
             xmlns="http://www.w3.org/2000/svg" 
-            width="16" 
-            height="16" 
+            width="35" 
+            height="17" 
             fill="red" 
             class="bi bi-heart-fill" 
             viewBox="0 0 16 16">
