8fc3224df581fa33628d821f7c33fd29a674eed8
DanaGolub <48007635+DanaGolub@users.noreply.github.com>
2022-01-06 15:55:05 -0800

    Adding to About pg

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -1,7 +1,9 @@
 function PageAbout() {
+    
     return (
         <section className="about-page">
             <h2>Page About</h2>
+            <h3>This product uses the TMDb API but is not endorsed or certified by TMDb.</h3>
         </section>
     )
 }
